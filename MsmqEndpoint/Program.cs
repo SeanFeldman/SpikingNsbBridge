8a0fae8dad54bd85b850b9cfb8c6cc7fff2491ae
Sean Feldman <feldman.sean@gmail.com>
2017-10-12 14:13:26 -0600

    allow both, commands and events for testing

--- MsmqEndpoint/Program.cs
+++ MsmqEndpoint/Program.cs
@@ -19,21 +19,32 @@ namespace MsmqEndpoint
             // todo: document that requires NServiceBus.Bridge.Connector
             var bridge = routing.ConnectToBridge("Bridge-Publisher");
             bridge.RouteToEndpoint(typeof(MyCommand), "Subscriber");
-
+            
             var endpoint = await Endpoint.Start(configuration).ConfigureAwait(false);
 
-            Console.WriteLine("Press <enter> to send a message");
+            Console.WriteLine("Press 'c' to send a command and 'e' to publish an event.");
             Console.WriteLine("Press any other key to exit");
-            while (true)
+            var @continue = true;
+
+            while (@continue)
             {
-                if (Console.ReadKey().Key != ConsoleKey.Enter)
+                var key = Console.ReadKey().Key;
+                switch (key)
                 {
-                    break;
+                    case ConsoleKey.C:
+                        await endpoint.Send(new MyCommand()).ConfigureAwait(false);
+                        Console.WriteLine("\nCommand sent");
+                        break;
+
+                    case ConsoleKey.E:
+                        await endpoint.Publish<MyEvent>(@event => { }).ConfigureAwait(false);
+                        Console.WriteLine("\nEvent sent");
+                        break;
+
+                    default:
+                        @continue = false;
+                        break;
                 }
-                //                await endpoint.Send(new MyCommand()).ConfigureAwait(false);
-                //                Console.WriteLine("Command sent");
-                await endpoint.Publish<MyEvent>(@event => { }).ConfigureAwait(false);
-                Console.WriteLine("Event sent");
             }
 
             await endpoint.Stop().ConfigureAwait(false);
